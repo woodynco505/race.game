bfa7ad23f358720ebb194b80edbdbf74992fd917
woodynco505 <94364433+woodynco505@users.noreply.github.com>
2021-11-17 03:26:06 +0900

    Add files via upload

--- init.js
+++ init.js
@@ -1,39 +1,37 @@
-const startClickeHandler = () => {
-    alert("asdklsadlk");
-};
-
 const comboDOM = document.getElementById("combo");
 const ball1DOM = document.getElementById("ball1");
 const ball2DOM = document.getElementById("ball2");
 const ball3DOM = document.getElementById("ball3");
 const ball4DOM = document.getElementById("ball4");
 
+const windowWidth = window.innerWidth;
+const boxWidth = 90;
+const limitWidth = windowWidth - boxWidth;
 
-
+const resetClickHandler = () => {
+    ball1DOM.style.marginLeft = "";
+    ball2DOM.style.marginLeft = "";
+    ball3DOM.style.marginLeft = "";
+    ball4DOM.style.marginLeft = "";
+};
 
 
 const startClickHandler  = () => {
     const selectValue = comboDOM.value;
-
-    //validation
+    
+ 
     if (selectValue === "") {
-        return alert ("공을선택해주세요")
+        return alert ("숫자를 선택해주세요")
     }
 
 
     setInterval(() => { 
-
         let ran1 = Math.random() *25 + 1;
         ran1 = Math.floor(ran1);
-
         let ran2 = Math.random() *25 + 1;
         ran2 = Math.floor(ran2);
-
-
         let ran3 = Math.random() *25 + 1;
         ran3 = Math.floor(ran3);
-
-
         let ran4 = Math.random() *25 + 1;
         ran4 = Math.floor(ran4);
     
@@ -46,9 +44,13 @@ const startClickHandler  = () => {
         let currentValue1 = ball1DOM.style.marginLeft;
         currentValue1 = currentValue1.split("p");
         currentValue1 = parseInt(currentValue1) + ran1;
+        if (currentValue1 <= limitWidth) {
         currentValue1 = currentValue1 + "px"
-        ball1DOM.style.marginLeft = currentValue1;
-    }
+        ball1DOM.style.marginLeft = currentValue1;}
+        }
+    
+
+
 
     if (ball2DOM.style.marginLeft === ""){
         ball2DOM.style.marginLeft = ran2 + "px";
@@ -56,9 +58,13 @@ const startClickHandler  = () => {
         let currentValue2 = ball2DOM.style.marginLeft;
         currentValue2 = currentValue2.split("p");
         currentValue2 = parseInt(currentValue2) + ran2;
+        if (currentValue2 <= limitWidth) {
         currentValue2 = currentValue2 + "px"
-        ball2DOM.style.marginLeft = currentValue2;
-    }
+        ball2DOM.style.marginLeft = currentValue2;}
+        }
+    
+
+
 
     if (ball3DOM.style.marginLeft === ""){
         ball3DOM.style.marginLeft = ran3 + "px";
@@ -66,9 +72,13 @@ const startClickHandler  = () => {
         let currentValue3 = ball3DOM.style.marginLeft;
         currentValue3 = currentValue3.split("p");
         currentValue3 = parseInt(currentValue3) + ran3;
+        if (currentValue3 <= limitWidth) {
         currentValue3 = currentValue3 + "px"
-        ball3DOM.style.marginLeft = currentValue3;
-    }
+        ball3DOM.style.marginLeft = currentValue3;}
+        }
+    
+
+
 
     if (ball4DOM.style.marginLeft === ""){
         ball4DOM.style.marginLeft = ran4 + "px";
@@ -76,8 +86,10 @@ const startClickHandler  = () => {
         let currentValue4 = ball4DOM.style.marginLeft;
         currentValue4 = currentValue4.split("p");
         currentValue4 = parseInt(currentValue4) + ran4;
+        if (currentValue4 <= limitWidth) {
         currentValue4 = currentValue4 + "px"
-        ball4DOM.style.marginLeft = currentValue4;
-    }
+        ball4DOM.style.marginLeft = currentValue4;}
+        }
+    
 }, 100);
 };
